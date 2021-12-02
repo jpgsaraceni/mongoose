66db00942183e090997751de535d2ec64bf689b3
jpgsaraceni <jpgomes@id.uff.br>
2021-12-02 16:14:16 -0300

    feat: Finish mongoose tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mongoose
+
+My first steps with NoSQL. This code was written following the tutorial on [Mongoose documentation](https://mongoosejs.com/docs/index.html)
---
+++ app.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+}
